03fc92da5541ef4f166482232aeb52ebbc9b56c9
winty <abajpai2001@gmail.com>
2024-04-30 17:52:12 +0530

    added route for qr code
    need to add a qr generator library

--- routes/redirectRoutes.js
+++ routes/redirectRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getExternalLink } = require('../controllers/redirectController');
 
+router.get('/:shortcode.qr',(req,res)=>{
+    res.send({qr:'done'});
+});
+
 router.get('/:shortcode', getExternalLink);
+
 
 module.exports = router;
