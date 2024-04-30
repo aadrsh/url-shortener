3192fd1279bf34d822fa3889ae9fe11a5d8a9a9a
Aadrsh Bajpai <abajpai2001@gmail.com>
2024-05-01 00:45:06 +0530

    Added QR
    adding .qr as suffix will create a qr of that url

--- migrations/20240428054931-create-links.js
+++ migrations/20240428054931-create-links.js
@@ -1,55 +1,53 @@
-'use strict';
-
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // Create the 'Links' table
-    await queryInterface.createTable('Links', {
+    await queryInterface.createTable("Links", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       originalUrl: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       shortenedUrl: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       alias: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: true,
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       deletedAt: {
         type: Sequelize.DATE,
-        allowNull: true
-      }
+        allowNull: true,
+      },
     });
 
     // Add a unique index to the 'shortenedUrl' column
-    await queryInterface.addIndex('Links', ['shortenedUrl'], {
+    await queryInterface.addIndex("Links", ["shortenedUrl"], {
       unique: true,
-      fields: ['shortenedUrl']
+      fields: ["shortenedUrl"],
     });
   },
 
   down: async (queryInterface, Sequelize) => {
     // Remove the unique index from the 'shortenedUrl' column
-    await queryInterface.removeIndex('Links', 'shortenedUrl');
-
+    await queryInterface.removeIndex("Links", "shortenedUrl");
     // Drop the 'Links' table
-    await queryInterface.dropTable('Links');
-  }
+    await queryInterface.dropTable("Links");
+  },
 };
--- routes/redirectRoutes.js
+++ routes/redirectRoutes.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const router = express.Router();
-const { getExternalLink } = require('../controllers/redirectController');
+const express = require("express");
+const qr = require("qrcode");
 
-router.get('/:shortcode.qr',(req,res)=>{
-    res.send({qr:'done'});
+const router = express.Router();
+const { getExternalLink } = require("../controllers/redirectController");
+
+router.get("/:shortcode.qr", (req, res) => {
+  const { shortcode } = req.params;
+  // Generate QR code for the long URL
+  qr.toDataURL("https://codedar.win/" + shortcode, (err, qrUrl) => {
+    if (err) {
+      console.error("Error generating QR code:", err);
+      res.status(500).send("Error generating QR code");
+    } else {
+      // Return the QR code image
+      res.send(`<img src="${qrUrl}" alt="QR Code"/>`);
+    }
+  });
 });
 
-router.get('/:shortcode', getExternalLink);
-
+router.get("/:shortcode", getExternalLink);
 
 module.exports = router;
